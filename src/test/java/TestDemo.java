71772b54aebb2f079893f31c71969a2d6922dc0a
ImOkkkk <547717253@qq.com>
2021-12-01 23:22:57 +0800

    feat: Kafka拦截器介绍及实现

--- src/test/java/TestDemo.java
+++ src/test/java/TestDemo.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -20,7 +23,11 @@ public class TestDemo {
         properties.put("value.serializer","org.apache.kafka.common.serialization.StringSerializer");
         // 开启GZIP压缩
         properties.put("compression.type","gzip");
-
+        List<String> interceptors = new ArrayList<>();
+        // 配置拦截器
+        interceptors.add("org.imokkkk.interceptors.AddTimestampInterceptor"); // 拦截器 1
+        interceptors.add("org.imokkkk.interceptors.UpdateCounterInterceptor"); // 拦截器 2
+        properties.put(ProducerConfig.INTERCEPTOR_CLASSES_CONFIG, interceptors);
         Producer<String,String> producer = new KafkaProducer<String, String>(properties);
     }
 }
