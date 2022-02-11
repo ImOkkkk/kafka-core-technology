325ca1a39756adf8276451b3b50f3e7a2ede6568
ImOkkkk <547717253@qq.com>
2022-02-11 21:59:41 +0800

    feat: Kafka消费者手动提交最佳实践

--- src/test/java/TestDemo.java
+++ src/test/java/TestDemo.java
@@ -1,6 +1,9 @@
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 import org.apache.kafka.clients.consumer.CommitFailedException;
@@ -8,6 +11,8 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.clients.consumer.OffsetCommitCallback;
 import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
@@ -15,6 +20,7 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.KafkaException;
+import org.apache.kafka.common.TopicPartition;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -75,27 +81,74 @@ public class TestDemo {
         properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
         consumer.subscribe(Arrays.asList("foo", "bar"));
-        while (true) {
-            ConsumerRecords<String, String> records = consumer.poll(100);
-            // 处理消息
-            process(records);
-            try {
-                consumer.commitAsync();
-            } catch (CommitFailedException e) {
-                // 处理提交失败异常
-                handle(e);
-            }
+//        while (true) {
+//            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+//            // 处理消息
+//            process(records);
+            // 同步提交
+//            try {
+//                consumer.commitAsync();
+//            } catch (CommitFailedException e) {
+//                // 处理提交失败异常
+//                handle(e);
+//            }
+
+            // 异步提交
+//            consumer.commitAsync(((offsets, exception) -> {
+//                if (exception != null){
+//                    handle(exception);
+//                }
+//            }));
+
 //            for (ConsumerRecord<String, String> record : records) {
 //                System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
 //            }
+//        }
+        // 同步提交、异步提交结合
+//        try {
+//            while (true){
+//                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+//                process(records);
+//                // 使用异步提交规避阻塞
+//                consumer.commitAsync();
+//            }
+//        }catch (Exception e){
+//            // 处理异常
+//            handle(e);
+//        }finally {
+//            try {
+//                // 最后一次提交使用同步阻塞式提交
+//                consumer.commitSync();
+//            }finally {
+//                consumer.close();
+//            }
+//        }
+        while (true){
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            int count = 0;
+            Map<TopicPartition, OffsetAndMetadata> offsets = new HashMap<>();
+            for (ConsumerRecord<String, String> record : records) {
+                // 处理消息
+                process(record);
+                offsets.put(new TopicPartition(record.topic(), record.partition()),
+                    new OffsetAndMetadata(record.offset() + 1));
+                if (count % 100 == 0) {
+                    consumer.commitAsync(offsets, null);
+                }
+                count++;
+            }
         }
     }
 
-    private void handle(CommitFailedException e) {
+    private void handle(Exception e) {
         
     }
 
     private void process(ConsumerRecords<String, String> records) {
         
+    }
+
+    private void process(ConsumerRecord<String, String> record) {
+
     }
 }
